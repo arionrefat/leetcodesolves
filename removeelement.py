1436e43d2dc028a5dc4dafcbe96bd41d1fc7dede
arionrefat <arionrefat@gmail.com>
2023-12-14 01:33:39 +0600

    solve remove element

--- removeelement.py
+++ removeelement.py
@@ -2,4 +2,15 @@ from typing import List
 
 
 def removeElement(nums: List[int], val: int) -> int:
-    return 9
+    counter = 0
+
+    orginalLength = len(nums)
+
+    for i in range(len(nums) - 1, -1, -1):
+        if nums[i] == val:
+            nums.remove(val)
+            counter += 1
+    return orginalLength - counter
+
+
+print(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
