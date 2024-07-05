e0cd2b88d0f9a8b1e854ec256a54141c3ea9f4a4
arionrefat <arionrefat@gmail.com>
2024-07-05 22:50:40 +0600

    added two more

---
+++ 104maxDepth.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # this is recursive solution
+    # the time complexity is O(n) where n is the number of nodes in the tree
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# this is the recursive BFS solution
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        q = deque([root])
+        level = 0
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+
+        return level
